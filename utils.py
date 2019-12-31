607075146a5da18a558e5895b36cc7f73d238ffa
sjf <sarah.fortune@gmail.com>
2019-12-31 11:21:16 -0800

    Add run with exit on failure

--- utils.py
+++ utils.py
@@ -1,6 +1,14 @@
 import sys
+import os
+from data.log import *
 
 def get_arg(i, default):
   if i < len(sys.argv):
     return sys.argv[i]
   return default
+
+def run(cmd):
+  log('Running', cmd)
+  res = os.system(cmd)
+  if res != 0:
+    sys.exit(res)
